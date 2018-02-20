009e10ee5eeff6bc156e33e0a8d5c9861bc6c89b
ryanml <rlanese@asu.edu>
2018-02-19 23:17:43 -0700

    CSS only solution for ledger publisher title overflow

--- app/renderer/components/preferences/payment/ledgerTable.js
+++ app/renderer/components/preferences/payment/ledgerTable.js
@@ -62,10 +62,6 @@ class LedgerTable extends ImmutableComponent {
     return urlUtil.getHostPattern(synopsis.get('publisherKey'))
   }
 
-  getDisplayedSiteName (siteName) {
-    return (siteName.length > 45 ? (siteName.substr(0, 45) + '...') : siteName)
-  }
-
   getVerifiedIcon (synopsis) {
     return <span className={css(
       styles.verified,
@@ -167,7 +163,6 @@ class LedgerTable extends ImmutableComponent {
     const percentage = pinned ? this.pinPercentageValue(synopsis) : synopsis.get('percentage')
     const publisherKey = synopsis.get('publisherKey')
     const siteName = synopsis.get('siteName')
-    const siteDisplayName = this.getDisplayedSiteName(siteName)
     const defaultAutoInclude = this.enabledForSite(synopsis)
 
     const rowRefName = 'rowPercentage_' + publisherKey
@@ -188,7 +183,7 @@ class LedgerTable extends ImmutableComponent {
                 ? <img className={css(styles.siteData__anchor__icon_favicon)} src={faviconURL} alt='' onError={this.onFaviconError.bind(null, faviconURL, publisherKey)} />
                 : <span className={css(styles.siteData__anchor__icon_default)}><span className={globalStyles.appIcons.defaultIcon} /></span>
             }
-            <span className={css(styles.siteData__anchor__url)} title={siteName} data-test-id='siteName'>{siteDisplayName}</span>
+            <span className={css(styles.siteData__anchor__url)} title={siteName} data-test-id='siteName'>{siteName}</span>
           </a>
         </div>,
         value: publisherKey
@@ -415,6 +410,13 @@ const styles = StyleSheet.create({
     display: 'flex',
     flex: '1',
     alignItems: 'center'
+  },
+
+  siteData__anchor: {
+    width: '430px',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap'
   },
 
   siteData__anchor__icon_favicon: {
